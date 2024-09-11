525117f7e311a4344b62996a546456751da89d16
Carla Congiu <44064718+carlacongiu@users.noreply.github.com>
2024-09-11 13:00:49 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,14 @@
 import streamlit as st
+#from vulgata_spacy import vulgata_spacy
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+#nlp = vulgata_spacy.VulgataSpaCy()
+text = st.text_area("Paste Text Here")
+annoy_bool = st.checkbox("Use Annoy Index")
+if annoy_bool:
+    max_distance = st.slider("Set Max Distance", .01, 1.0, .5)
+    style = st.selectbox("Run Annoy on:", ["sent", "ent"])
+#if text != "":
+ #   doc = nlp.create_doc(text)
+  #  if annoy_bool:
+   #     doc = nlp.annoy_matcher(style=style, max_distance=max_distance)
+    #nlp.visualize_doc()
